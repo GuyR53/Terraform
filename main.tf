2822b30cc7f96b18278952787f34eadc09664c35
Guy Royburt <guyr@Guys-MacBook-Air.local>
2022-03-23 20:50:15 +0200

    added providers.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-  }
 
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {}
-}
 
 module "Network" {
   source = "./modules/Network"
